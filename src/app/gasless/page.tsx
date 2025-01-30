e4f441d6d0eec6e3c2f3b714f91604bc11f6bbb9
YoannRDC <yoann.radenac@gmail.com>
2025-01-30 16:30:39 +0100

    Correcting loop v3

--- src/app/gasless/page.tsx
+++ src/app/gasless/page.tsx
@@ -53,7 +53,7 @@ const GaslessHome: React.FC = () => {
 		<div className="flex flex-col items-center">
 
 			<h1 className="text-2xl md:text-6xl font-semibold md:font-bold tracking-tighter mb-12 text-zinc-100">
-				Sponsored Transactions YR Tests 6
+				Sponsored Transactions YR Tests 7
 			</h1>
  			<ConnectButton
 				client={client}
@@ -151,7 +151,6 @@ const GaslessHome: React.FC = () => {
 					</>
 				)}
 			</div>
-
 
 			<Link href={"/"} className="text-sm text-gray-400 mt-8">
 				Back to menu
