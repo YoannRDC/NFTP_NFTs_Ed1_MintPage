1a3cc75450f85781d89194ffa49ba44d031433b7
YoannRDC <yoann.radenac@gmail.com>
2025-02-08 20:29:50 +0100

    Video presentation added v2

--- src/app/components/NFTP_presentation.tsx
+++ src/app/components/NFTP_presentation.tsx
@@ -2,7 +2,7 @@ export default function VideoPresentation() {
     return (
       <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0, overflow: 'hidden' }}>
         <iframe
-          src="https://youtube.com/shorts/jtMNejMvX0k"
+          src="https://www.youtube.com/embed/jtMNejMvX0k"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
           style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
