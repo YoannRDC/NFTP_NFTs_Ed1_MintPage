686b4254fba5ae4ec64f2d359842fc3834f3c315
YoannRDC <yoann.radenac@gmail.com>
2025-02-12 12:43:27 +0100

    Stripe implementation v4

--- src/app/api/stripe-intent/route.ts
+++ src/app/api/stripe-intent/route.ts
@@ -6,7 +6,7 @@ import Stripe from "stripe";
 export async function POST(req: Request) {
   const { buyerWalletAddress } = await req.json();
 
-  const stripe = new Stripe(process.env.STRIPE_WEBHOOK_SECRET as string, {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
     apiVersion: "2025-01-27.acacia",
   });
   
