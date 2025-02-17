7e7bdd9e4c2625f4ba6126aff20165e43df1af0a
YoannRDC <yoann.radenac@gmail.com>
2025-02-17 11:01:51 +0100

    css debordement phone v2

--- src/app/components/NFTP_presentation.tsx
+++ src/app/components/NFTP_presentation.tsx
@@ -1,12 +1,12 @@
 export default function VideoPresentation() {
     return (
-      <div style={{ position: 'relative', height: "300px", width: "500px" }}>
+      <div style={{ position: 'relative' }}>
         <iframe
           src="https://youtube.com/embed/i3-5yO6GXw0?rel=0&modestbranding=1&autoplay=0"
           loading="lazy"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
-          style={{ position: 'relative', top: 10, left: 0, width: '100%', height: '100%' }}
+          style={{ position: 'relative', top: 10, left: 0, width: '80%', height: '80%' }}
           title="Présentation NFT Propulsion"
         ></iframe>
       </div>
