3a31f1ecf32773fe2e2baa9dfc9ecab227599e89
YoannRDC <yoann.radenac@gmail.com>
2025-02-25 21:38:35 +0100

    Webhook v33

--- src/app/api/stripe-webhook/route.ts
+++ src/app/api/stripe-webhook/route.ts
@@ -3,7 +3,8 @@ import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 import contractABI from "../../../../contracts/contract_NFTP_ed1_ABI.json";
-import { getActiveClaimCondition } from "thirdweb/extensions/erc721";
+import { claimTo, getActiveClaimCondition } from "thirdweb/extensions/erc721";
+import { ContractOptions } from "thirdweb/contract";
 
 export async function POST(req: NextRequest) {
   const body = await req.text();
@@ -42,15 +43,23 @@ export async function POST(req: NextRequest) {
         { secretKey: process.env.THIRDWEB_API_SECRET_KEY }
       );      
 
-
-
-
       const contract = await sdk.getContract(nftContractAddress, contractABI);
       console.log("metadata:", contract.metadata);
       console.log("owner:", contract.owner);
       console.log("chainId:", contract.chainId);
 
-      const dropContract = await sdk.getContract(nftContractAddress, "nft-drop");
+      const transaction = claimTo({
+        contract: contract as unknown as Readonly<ContractOptions<any, `0x${string}`>>,
+        to: buyerWalletAddress,
+        quantity: 1n,
+        from: "0x6debf5C015f0Edd3050cc919A600Fb78281696B9", // address of the one claiming
+      });
+      console.log("NFT claimed successfully:", transaction);
+
+      console.log("Another way");
+
+
+/*       const dropContract = await sdk.getContract(nftContractAddress, "nft-drop");
       const activeClaimCondition = await dropContract.claimConditions.getActive();
 
       const claimToOptions = {
@@ -59,7 +68,7 @@ export async function POST(req: NextRequest) {
       };
 
       const tx = await contract.erc721.claimTo(buyerWalletAddress, 1, claimToOptions);
-      console.log("NFT claimed successfully:", tx);
+      console.log("NFT claimed successfully:", tx); */
     }
 
     return NextResponse.json({ message: "OK" });
