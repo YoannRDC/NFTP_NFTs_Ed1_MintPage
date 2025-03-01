73688c82167238ed2b2a92779bde8c03ecf4e7c7
YoannRDC <yoann.radenac@gmail.com>
2025-03-01 20:00:23 +0100

    Conv Eur to POL v2

--- src/app/components/PurchasePage.tsx
+++ src/app/components/PurchasePage.tsx
@@ -25,10 +25,6 @@ export default function PurchasePage({
   const smartAccount = useActiveAccount();
   const [clientSecret, setClientSecret] = useState<string>("");
 
-  console.log("requestedQuantity:", requestedQuantity);
-  console.log("amount:", amount);
-  console.log("stripeMode:", stripeMode);
-
   // Choix de la clé publishable en fonction du mode
   const stripePublishableKey =
     stripeMode === "live"
