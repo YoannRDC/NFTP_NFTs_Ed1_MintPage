5cfd8ac15b6c57468b4ac286c49103b663636222
YoannRDC <yoann.radenac@gmail.com>
2025-04-11 15:15:10 +0200

    Debugging refacto

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        // Appliquer le header à toutes les routes
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Permissions-Policy',
+            // Autoriser picture-in-picture pour le domaine self (votre site) et youtube.com
+            value: 'picture-in-picture=(self "https://www.youtube.com")',
+          },
+        ],
+      },
+    ];
+  },
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
